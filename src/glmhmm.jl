2c642b24007059fa982c245c43ad19f5095e2510
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2022-10-24 06:34:41 -0400

    Update glmhmm.jl

--- src/glmhmm.jl
+++ src/glmhmm.jl
@@ -6,9 +6,9 @@ include("utils.jl")
 
 struct forward_object
     α::Matrix
-    π::Array
+    π::Vector
     Ψ::Matrix
-    Z::Array
+    Z::Vector
 end
 
 function forward_message!(fobj, dm_obj)
@@ -31,25 +31,21 @@ end
 
 struct backward_object
     β::Matrix
-    Ψ::Matrix
 end
 
-function backward_message!(bobj, dm_obj)
+function backward_message!(bobj, dm_obj, Ψ)
     T = size(dm_obj.L, 1)
     bobj.β[T, :] .= 1.
     for t in T:-1:2
-        βt, _ = normalize_z(bobj.Ψ * (dm_obj.L[t, :] .* bobj.β[t, :]))
+        βt, _ = normalize_z(Ψ * (dm_obj.L[t, :] .* bobj.β[t, :]))
         bobj.β[t-1, :] = βt
     end
 end
 
-function update_backward_message!(bobj, pos)
-    bobj.Ψ[:, :] = pos.Ψ
-end
 
 struct data_models
-    y::Array
-    ϕ::Array
+    y::Vector
+    ϕ::Vector
     L::Matrix
 end
 
@@ -70,10 +66,11 @@ function compute_likelihoods!(dm_obj, params)
 end
 
 struct posterior_object
-    μ::Array
-    σ::Array
+    W::Matrix
+    μ::Vector
+    σ::Vector
     γ::Matrix
-    π::Array
+    π::Vector
     Ψ::Matrix
 end
 
@@ -81,21 +78,35 @@ function compute_posteriors(fobj, bobj, dm_obj)
     T = size(dm_obj.y, 1)
     K = length(dm_obj.ϕ)
     γ = fobj.α .* bobj.β
-    μ = zeros(K)
+    W = zeros(T, K)
+    
     for k in 1:K
-        μ[k] = γ[:, k]'y ./ sum(γ[:, k])
+        p_z = diagm(γ ./ sum(γ[:, k]))
+        X_proj = dm_obj.X' * p_z * dm_obj.X
+        y_proj = dm_obj.X' * p_z * dm_obj.y
+        W[:, k] = X_proj \ y_proj
     end
+    
+    μ = dm_obj.X * W
+        
     y_minus_μ = add_dim(y) .- add_dim(μ)'
+        
     σ2, ξ = zeros(K), zeros(K, K, T)
+        
     # lets look into fixing this
     for k in 1:K
         σ2[k] = γ[:, k]'y_minus_μ[:, k].^2 / sum(γ[:, k])
     end
+    
     π = γ[1, :] ./ sum(γ[1, :])
+    
     @inbounds for t in 1:T-1
         ξ[:, :, t] = (bobj.Ψ .* (fobj.α[t, :] * (dm_obj.L[t+1, :] .* bobj.β[t+1, :])')) ./ fobj.Z[t+1]
     end
+    
     ξ_NT = drop_dim(sum(ξ; dims=(3, 2)))
+    
     ξ = drop_dim(sum(ξ; dims=3)) ./ ξ_NT
-    return posterior_object(μ, sqrt.(σ2), γ, π, ξ)
+    
+    return posterior_object(W, μ, sqrt.(σ2), γ, π, ξ)
 end
